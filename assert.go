f24729f28fb9bc5ab1615beeacf00854c61f08a7
hau <ngtranhau@gmail.com>
2020-08-18 11:21:37 +0700

    chg: add assert True, False

--- assert.go
+++ assert.go
@@ -8,3 +8,11 @@ func Bool(want bool, got bool, msg string) {
 		log.Fatalf("assert error: %s", msg)
 	}
 }
+
+func True(got bool, msg string) {
+	Bool(true, got, msg)
+}
+
+func False(got bool, msg string) {
+	Bool(false, got, msg)
+}
